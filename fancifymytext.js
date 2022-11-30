6abe4c67e685f52e0cf816f53413b25c49bc8d91
Sirapicha <sawettanants@gmail.com>
2022-11-29 20:14:24 -0800

    Assignment 4

---
+++ fancifymytext.js
@@ -0,0 +1,26 @@
+function bigger() {
+	document.getElementById("textarea").style.fontSize="24pt";
+}
+
+function bold() {
+	alert("Text will set to bold blue.");
+	document.getElementById("textarea").style.fontWeight="bold";
+	document.getElementById("textarea").style.color="blue";
+	document.getElementById("textarea").style.textDecoration="underline";
+}
+
+function normal() {
+	alert("Text will set to normal.");
+	document.getElementById("textarea").style.fontWeight="normal";
+	document.getElementById("textarea").style.color="black";
+	document.getElementById("textarea").style.textDecoration="none";
+}
+
+function moo(){
+    	document.getElementById("textarea").style.textTransform="uppercase";
+	
+	var parts = document.getElementById("textarea").value.split(".");
+	var str = parts.join("-Moo");
+
+	document.getElementById("textarea").value = str;
+}
