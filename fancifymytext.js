159181293860b842825a1dd00cbf72b07591d06e
Sirapicha <93172167+Sirapicha@users.noreply.github.com>
2022-11-30 12:19:22 -0800

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,23 +1,23 @@
 function bigger() {
+	alert("Text will get bigger!");
 	document.getElementById("textarea").style.fontSize="24pt";
 }
 
-function bold() {
-	alert("Text will set to underlined bold blue.");
-	document.getElementById("textarea").style.fontWeight="bold";
-	document.getElementById("textarea").style.color="blue";
-	document.getElementById("textarea").style.textDecoration="underline";
+function changeStyle() {
+	alert("Text will get styling");
+
+	if (document.getElementById("fancyShmancy").checked) {
+		document.getElementById("textarea").style.fontWeight="bold";
+		document.getElementById("textarea").style.color="blue";
+		document.getElementById("textarea").style.textDecoration="underline";
+	}
+	else if (document.getElementById("boringBetty").checked) {
+		document.getElementById("textarea").style.fontWeight="normal";
+	}
 }
 
-function normal() {
-	alert("Text will set to normal.");
-	document.getElementById("textarea").style.fontWeight="normal";
-	document.getElementById("textarea").style.color="black";
-	document.getElementById("textarea").style.textDecoration="none";
-}
-
-function moo(){
-    	document.getElementById("textarea").style.textTransform="uppercase";
+function moo() {
+	document.getElementById("textarea").style.textTransform="uppercase";
 	
 	var parts = document.getElementById("textarea").value.split(".");
 	var str = parts.join("-Moo");
